0779f52f138960e24a954d3f55cfb61c68a6d4ad
Rathin7 <rathinbaraiRB637@gmail.com>
2023-05-02 22:37:44 +0600

    Added tosta message on the recipe details

--- src/layouts/pages/Details/Details.jsx
+++ src/layouts/pages/Details/Details.jsx
@@ -4,28 +4,34 @@ import { FaHeart} from 'react-icons/fa';
 import ShowRecipe from './ShowRecipe';
 const Details = () => {
     const chefDetails = useLoaderData();
-    const { chefImg, chefName, experiences, chefLike, id, recipes } = chefDetails;
+    const { chefImg, chefName, experiences, chefLike, id, recipes,chefBio } = chefDetails;
     return (
         <div>
             <div className='mt-6 md:w-9/12 mx-auto flex justify-between items-center'>
                 <div className='w-5/12 p-4 card  bg-base-100 shadow-xl  border rounded-md'>
                 <img className='animate-pulse rounded-2xl shadow-xl' src={chefImg} alt="" />
                 </div>
-                <div className='card  bg-base-100 shadow-xl p-4 rounded-md'>
+                <div className='card w-[60%]  bg-base-100 shadow-xl p-4 rounded-md'>
                     <h1 className='text-2xl font-bold'>Name:{chefName}</h1>
                     <p className='text-xl font-bold mt-2'>Experiences: <span className=' text-indigo-700'>{experiences}</span></p>
+                    <p className='text-xl font-bold mt-2'>Bio: <span className=' text-gray-600  text-sm'>{chefBio}</span></p>
                     <p className='flex  items-center gap-2 text-xl mt-2 font-bold'>
                         
                         Like:<span className='text-indigo-700'>{chefLike}</span><FaHeart className='text-red-600'/></p>
                     <p className='text-xl mt-2 font-bold'>Total recipes:<span className=' text-indigo-700'>{recipes.length}</span> </p>
                 </div>
             </div>
-            {
+            <div >
+                <h1 className='text-center font-semibold text-3xl my-9'>Chef Recipes</h1>
+                <div className='w-11/12 mx-auto grid grid-cols-3 gap-3'>
+                {
                 recipes.map(recipe => <ShowRecipe
                     key={recipe.recipesId}
                     recipe={recipe}
                 ></ShowRecipe>)
             }
+           </div>
+           </div>
         </div>
     );
 };
--- src/layouts/pages/Details/ShowRecipe.jsx
+++ src/layouts/pages/Details/ShowRecipe.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { FaStar } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const ShowRecipe = ({ recipe }) => {
-    console.log(recipe);
+    const notify = () => toast("Added to Favorite");
     return (
-        <div>
-            <h1>All recipe is here name:{recipe ?.recipesName}</h1>
+        <div className="mt-12 card bg-base-100 shadow-xl ">
+            
+            <figure>
+                <img className='rounded-full w-9/12 mx-auto' src={recipe?.img} alt="" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{recipe?.recipesName}</h2>
+                <p>{recipe?.shortDescription}</p>
+                <p className='text-blue-600 font-semibold'>Ingredients Blow</p>
+                {
+                    recipe?.ingredients.map(ing => <li>{ing}</li>)
+                }
+                <p className='flex items-center justify-start gap-1'><FaStar className='text-yellow-500' />
+                    <FaStar className='text-yellow-500' />
+                    <FaStar className='text-yellow-500' />
+                    <FaStar className='text-yellow-500' />
+                    <FaStar className='text-yellow-500'/>{recipe?.ratings}</p>
+                <div className="card-actions justify-center">
+                    <button onClick={notify} className="w-full text-blue-600 font-bold text-center border border-blue-600 rounded-3xl px-3 shadow-md hover:bg-blue-400 hover:text-white hover:border-none">Add Favorite</button>
+                    <ToastContainer />
+                </div>
+            </div>
         </div>
     );
 };
